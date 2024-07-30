2b238d8a0b6e4eda593fbbf2f90ca59b9daecaab
Jim Garbe <jimgarbe@gmail.com>
2024-07-30 16:14:51 -0400

    Add files via upload

---
+++ css/plaintheme.php
@@ -0,0 +1,29 @@
+<?php
+  // Define application constants
+
+  define('THEME_CSS', 'css/plaintheme/three.css');
+  define('LOGO', 'css/plaintheme/images/logo.jpg');
+
+  define('BG_COLOR', 'black');
+
+// tab
+	define('A_COLOR', '#7CFF66');  // regular tab text
+	define('TAB', 'css/plaintheme/images/slate.gif');
+	define('BG_TAB', 'css/plaintheme/images/slate2.gif');
+
+ //clicked tab text
+	define('AP_COLOR', '#FFEE2F'); // clicked tab text
+	define('BG_VTAB', 'css/plaintheme/images/slate3.gif');
+
+
+
+
+  define('WARN_COLOR', 'red');
+  define('RCH_COLOR', 'red');
+  define('YCH_COLOR', 'yellow');
+
+
+// The archived_chsh.php 
+	define('HEAD', 'background-color:#308D21;color:white');
+	
+?>
